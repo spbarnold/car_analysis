1f6e6473e4c0235912e9e017c49a0c23ba867bc6
spbarnold <spbarnold@gmail.com>
2023-04-23 12:47:02 -0400

    updated app.y based on feedback

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # car_analysis
 Practicum Sprint 4 Project
 
+This analysis will look at the posting of used car ads in 2018 and 2019 with the purpose to evaluating how the market has changed betweem the two years.
+The original data can be found at https://practicum-contents.s3.us.west-1.amazonaws.com/datasets/vechicles_us.csv
+
+
 Prequisites
 None
 Dependancies 
 None
 
-In order to run the app locally, use streamlit run app.py. The app will be availiable at https://practicum-sprint-4-car-analysis.onrender.com/
+In order to run the app locally, use streamlit run app.py. 
+
+The app will be availiable at https://practicum-sprint-4-car-analysis.onrender.com/
--- app.py
+++ app.py
@@ -2,30 +2,58 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.title('Analysis of Used Car Ads 2018-2019')
+
 df = pd.read_csv('vehicles_us.csv')
-#seperates manufacturer from overall model field
-df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
-#creates column of the year of the posting
-df['year'] = pd.DatetimeIndex(df['date_posted']).year
-#calculates the age of the vehicle at time of posting
-df['age'] = df['year'] - df['model_year']
-#converting year to string for plotly-express
-df['year'] = df['year'].apply(str)
+#calculating the mean of the model year
+med_mod_year = df['model_year'].median()
+#calculating the mean of the odometer column
+mean_odometer = df['odometer'].median()
+
 
 #creates text header above the dataframe
 st.header('Data viewer')
+#replace null values in model_year
+df['model_year'] = df['model_year'].fillna(med_mod_year)
+#replacing all null values in 'cylinders'
+df['cylinders'] = df['cylinders'].fillna(0)
+#replacing null values with  unkown in paint_color
+df['paint_color'] = df['paint_color'].fillna('Unkown')
+#replacing all null values with 0
+df['is_4wd'] = df['is_4wd'].fillna(0)
+#replacing the null values of odometer with the mean value
+df['odometer'] = df['odometer'].fillna(mean_odometer)
+#converting float64 to int64 for all data expected to be treated as whole numbers
+df['model_year'] = df['model_year'].apply(int)
+df['days_listed'] = df['days_listed'].apply(int)
+#converting date_posted to date time data type
+df['date_posted'] = pd.to_datetime(df['date_posted'], format='%Y-%m-%d')
+#creating column for car manufacturer
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+#extracting year that the car was posted
+df['year'] = pd.DatetimeIndex(df['date_posted']).year
+#calculating the age of the car
+df['age'] = df['year'] - df['model_year']
+#converting year to string for plotly-express
+df['year'] = df['year'].apply(str)
 #displays dataframe using streamlit
 st.dataframe(df)
 
 st.header('Car Price by Age')
 st.write(px.scatter (df, x='age', y='price', color='year', 
                       color_discrete_sequence = ['navy', 'darkorange'],
-                    ))
+                      ))
 
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
 st.write('Fuel Type of Cars Posted by Year')
 st.write(px.histogram(df, x='fuel',
                       color = 'year',
-                      color_discrete_sequence = ['navy', 'darkorange'],
+                      color_discrete_sequence = ['navy', 'darkorange'],  
+                      histnorm=histnorm
                        ))
 
 st.write('Compare Prices of Cars versus Fuel Type')
